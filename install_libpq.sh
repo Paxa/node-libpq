abdee219c408bc5d16758581c9078172670d2569
Pavel Evstigneev <pavel.evst@gmail.com>
2019-06-08 02:11:08 +0800

    Try to install own openssl and libpq

---
+++ install_libpq.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+POSTGRES_VERSION="11.3"
+POSTGRES_DIR="${pwd}/vendor/postgres-${POSTGRES_VERSION}"
+OPENSSL_DIR="${pwd}/vendor/openssl-${OPENSSL_VERSION}"
+TMP_DIR="/tmp/postgres"
+JOBS="-j$(nproc || echo 1)"
+
+if [ -d "${TMP_DIR}" ]; then
+  rm -rf "${TMP_DIR}"
+fi
+
+mkdir -p "${TMP_DIR}"
+
+curl https://ftp.postgresql.org/pub/source/v${POSTGRES_VERSION}/postgresql-${POSTGRES_VERSION}.tar.gz | \
+  tar -C "${TMP_DIR}" -xzf -
+
+cd "${TMP_DIR}/postgres-${POSTGRES_VERSION}"
+
+if [ -d "${POSTGRES_DIR}" ]; then
+  rm -rf "${POSTGRES_DIR}"
+fi
+
+mkdir -p $POSTGRES_DIR
+./configure --prefix=$POSTGRES_DIR --with-openssl --without-readline
+
+cd src/interfaces/libpq; make; make install; cd -
+cd src/bin/pg_config; make install; cd -
+cd src/backend; make generated-headers; cd -
+cd src/include; make install; cd -
---
+++ install_openssl.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+if [ ${#} -lt 1 ]; then
+  echo "OpenSSL version required." 1>&2
+  exit 1
+fi
+
+OPENSSL_VERSION="${1}"
+OPENSSL_DIR="${pwd}/vendor/openssl-${OPENSSL_VERSION}"
+TMP_DIR="/tmp/openssl"
+JOBS="-j$(nproc || echo 1)"
+
+if [ -d "${TMP_DIR}" ]; then
+  rm -rf "${TMP_DIR}"
+fi
+mkdir -p "${TMP_DIR}"
+
+curl https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz | \
+  tar -C "${TMP_DIR}" -xzf -
+
+cd "${TMP_DIR}/openssl-${OPENSSL_VERSION}"
+
+if [ -d "${OPENSSL_DIR}" ]; then
+  rm -rf "${OPENSSL_DIR}"
+fi
+
+./Configure \
+  --prefix=${OPENSSL_DIR} \
+  enable-crypto-mdebug enable-crypto-mdebug-backtrace \
+  linux-x86_64
+
+make -s $JOBS
+make install_sw
+
+export PATH="${OPENSSL_DIR}/bin:${PATH}"
+export CFLAGS="-I${OPENSSL_DIR}/include"
+export LDFLAGS="-L${OPENSSL_DIR}/lib"
+export LD_LIBRARY_PATH="${OPENSSL_DIR}/lib"
+export PKG_CONFIG_PATH="${OPENSSL_DIR}/lib/pkgconfig"
+
+ls -lah $OPENSSL_DIR
